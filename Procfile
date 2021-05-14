c614c2d92550989a561a7bb232be204d1e361bd6
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-05-14 20:17:24 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,2 @@
+worker: python3 -m MashaRoBot
+ps:scale worker=1
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE Masha Robot
+:: Enables virtual env mode and then starts Masha
+env\scripts\activate.bat && py -m MashaRoBot
