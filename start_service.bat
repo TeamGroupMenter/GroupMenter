3646aec0d6ee54bacaa0c51307eed03bad02d1a7
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-05-15 10:03:40 +0530

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -27,5 +27,5 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it 
-net stop MashaRoBot
-net start MashaRoBot
+net stop  GroupMenter
+net start GroupMenter
