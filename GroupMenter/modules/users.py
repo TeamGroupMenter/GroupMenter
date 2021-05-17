39a8b2f1643c78bfae71c2cbb1e6171ccaf7ea78
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-05-17 13:16:50 +0530

    Update users.py

--- GroupMenter/modules/users.py
+++ GroupMenter/modules/users.py
@@ -11,10 +11,10 @@ from telegram.ext import (
     run_async,
 )
 
-import MashaRoBot.modules.sql.users_sql as sql
-from MashaRoBot import DEV_USERS, LOGGER, OWNER_ID, dispatcher
-from MashaRoBot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
-from MashaRoBot.modules.sql.users_sql import get_all_users
+import GroupMenter.modules.sql.users_sql as sql
+from GroupMenter import DEV_USERS, LOGGER, OWNER_ID, dispatcher
+from GroupMenter.modules.helper_funcs.chat_status import dev_plus, sudo_plus
+from GroupMenter.modules.sql.users_sql import get_all_users
 
 USERS_GROUP = 4
 CHAT_GROUP = 5
