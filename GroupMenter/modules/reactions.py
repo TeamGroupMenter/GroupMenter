d3f42b038da05b15a4334d783370071bb0806fb7
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-05-17 13:35:13 +0530

    Update reactions.py

--- GroupMenter/modules/reactions.py
+++ GroupMenter/modules/reactions.py
@@ -1,7 +1,7 @@
 import random
 
-from MashaRoBot import dispatcher
-from MashaRoBot.modules.disable import DisableAbleCommandHandler
+from GroupMenter import dispatcher
+from GroupMenter.modules.disable import DisableAbleCommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
