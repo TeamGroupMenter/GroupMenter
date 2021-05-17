aef38489a8b34c85d255e3798aaac16463d02ee3
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-05-17 17:59:30 +0530

    Update connection.py

--- GroupMenter/modules/connection.py
+++ GroupMenter/modules/connection.py
@@ -5,10 +5,10 @@ from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton, Upda
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import CommandHandler, CallbackQueryHandler, run_async
 
-import MashaRoBot.modules.sql.connection_sql as sql
-from MashaRoBot import dispatcher, DRAGONS, DEV_USERS
-from MashaRoBot.modules.helper_funcs import chat_status
-from MashaRoBot.modules.helper_funcs.alternate import send_message, typing_action
+import GroupMenter.modules.sql.connection_sql as sql
+from GroupMenter import dispatcher, DRAGONS, DEV_USERS
+from GroupMenter.modules.helper_funcs import chat_status
+from GroupMenter.modules.helper_funcs.alternate import send_message, typing_action
 
 user_admin = chat_status.user_admin
 
