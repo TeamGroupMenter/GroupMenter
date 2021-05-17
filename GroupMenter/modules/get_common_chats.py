150e1595b58d5ba22c0d7eb8a34a733d460bda8c
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-05-17 17:43:24 +0530

    Update get_common_chats.py

--- GroupMenter/modules/get_common_chats.py
+++ GroupMenter/modules/get_common_chats.py
@@ -1,9 +1,9 @@
 import os
 from time import sleep
 
-from MashaRoBot import OWNER_ID, dispatcher
-from MashaRoBot.modules.helper_funcs.extraction import extract_user
-from MashaRoBot.modules.sql.users_sql import get_user_com_chats
+from GroupMenter import OWNER_ID, dispatcher
+from GroupMenter.modules.helper_funcs.extraction import extract_user
+from GroupMenter.modules.sql.users_sql import get_user_com_chats
 from telegram import Update
 from telegram.error import BadRequest, RetryAfter, Unauthorized
 from telegram.ext import CallbackContext, CommandHandler, Filters
