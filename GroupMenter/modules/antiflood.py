e2a17b1a3dcfe48b1acb04e9a4d700d93714605e
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-05-17 18:18:31 +0530

    Update antiflood.py

--- GroupMenter/modules/antiflood.py
+++ GroupMenter/modules/antiflood.py
@@ -4,15 +4,15 @@ import re
 
 from telegram import Message, Chat, Update, User, ChatPermissions
 
-from MashaRoBot import TIGERS, WOLVES, dispatcher
-from MashaRoBot.modules.helper_funcs.chat_status import (
+from GroupMenter import TIGERS, WOLVES, dispatcher
+from GroupMenter.modules.helper_funcs.chat_status import (
     bot_admin,
     is_user_admin,
     user_admin,
     user_admin_no_reply,
 )
-from MashaRoBot.modules.log_channel import loggable
-from MashaRoBot.modules.sql import antiflood_sql as sql
+from GroupMenter.modules.log_channel import loggable
+from GroupMenter.modules.sql import antiflood_sql as sql
 from telegram.error import BadRequest
 from telegram.ext import (
     CallbackContext,
@@ -23,10 +23,10 @@ from telegram.ext import (
     run_async,
 )
 from telegram.utils.helpers import mention_html, escape_markdown
-from MashaRoBot.modules.helper_funcs.string_handling import extract_time
-from MashaRoBot.modules.connection import connected
-from MashaRoBot.modules.helper_funcs.alternate import send_message
-from MashaRoBot.modules.sql.approve_sql import is_approved
+from GroupMenter.modules.helper_funcs.string_handling import extract_time
+from GroupMenter.modules.connection import connected
+from GroupMenter.modules.helper_funcs.alternate import send_message
+from GroupMenter.modules.sql.approve_sql import is_approved
 
 FLOOD_GROUP = 3
 
