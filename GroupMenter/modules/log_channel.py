2d101ff33472fcd3a112c38753ae65628cfe61ec
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-05-17 17:23:59 +0530

    Update log_channel.py

--- GroupMenter/modules/log_channel.py
+++ GroupMenter/modules/log_channel.py
@@ -3,7 +3,7 @@ from functools import wraps
 
 from telegram.ext import CallbackContext
 
-from MashaRoBot.modules.helper_funcs.misc import is_module_loaded
+from GroupMenter.modules.helper_funcs.misc import is_module_loaded
 
 FILENAME = __name__.rsplit(".", 1)[-1]
 
@@ -13,9 +13,9 @@ if is_module_loaded(FILENAME):
     from telegram.ext import CommandHandler, JobQueue, run_async
     from telegram.utils.helpers import escape_markdown
 
-    from MashaRoBot import EVENT_LOGS, LOGGER, dispatcher
-    from MashaRoBot.modules.helper_funcs.chat_status import user_admin
-    from MashaRoBot.modules.sql import log_channel_sql as sql
+    from GroupMenter import EVENT_LOGS, LOGGER, dispatcher
+    from GroupMenter.modules.helper_funcs.chat_status import user_admin
+    from GroupMenter.modules.sql import log_channel_sql as sql
 
     def loggable(func):
         @wraps(func)
