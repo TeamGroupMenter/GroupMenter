33266d819294923f04e2ff94464e65fc8e25fbed
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-05-17 18:29:25 +0530

    Update _pyrocovid.py

--- GroupMenter/modules/_pyrocovid.py
+++ GroupMenter/modules/_pyrocovid.py
@@ -1,7 +1,7 @@
-from MashaRoBot import pbot as app
-from MashaRoBot.pyrogramee.errors import capture_err
-from MashaRoBot.pyrogramee.json_prettify import json_prettify
-from MashaRoBot.pyrogramee.fetch import fetch
+from GroupMenter import pbot as app
+from GroupMenter.pyrogramee.errors import capture_err
+from GroupMenter.pyrogramee.json_prettify import json_prettify
+from GroupMenter.pyrogramee.fetch import fetch
 from pyrogram import filters
 
 
