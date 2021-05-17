13bd0d6edf875f30ac5428b7f7b93e2505abf315
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-05-17 13:28:04 +0530

    Update stickers.py

--- GroupMenter/modules/stickers.py
+++ GroupMenter/modules/stickers.py
@@ -11,8 +11,8 @@ from telegram import TelegramError, Update
 from telegram.ext import run_async, CallbackContext
 from telegram.utils.helpers import mention_html
 
-from MashaRoBot import dispatcher
-from MashaRoBot.modules.disable import DisableAbleCommandHandler
+from GroupMenter import dispatcher
+from GroupMenter.modules.disable import DisableAbleCommandHandler
 
 combot_stickers_url = "https://combot.org/telegram/stickers?q="
 
