ff221e9baf70657fa05bbd2426194e643dee2470
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-05-17 13:35:55 +0530

    Update purge.py

--- GroupMenter/modules/purge.py
+++ GroupMenter/modules/purge.py
@@ -1,8 +1,8 @@
 import time
 from telethon import events
 
-from MashaRoBot import telethn
-from MashaRoBot.modules.helper_funcs.telethn.chatstatus import (
+from GroupMenter import telethn
+from GroupMenter.modules.helper_funcs.telethn.chatstatus import (
     can_delete_messages,
     user_is_admin,
 )
