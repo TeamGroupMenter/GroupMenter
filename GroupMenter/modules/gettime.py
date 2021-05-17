4ce8dd39a9346edde56d4759ff31b0878f554264
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-05-17 17:27:56 +0530

    Update gettime.py

--- GroupMenter/modules/gettime.py
+++ GroupMenter/modules/gettime.py
@@ -2,8 +2,8 @@ import datetime
 from typing import List
 
 import requests
-from MashaRoBot import TIME_API_KEY, dispatcher
-from MashaRoBot.modules.disable import DisableAbleCommandHandler
+from GroupMenter import TIME_API_KEY, dispatcher
+from GroupMenter.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
