dd96660b26358e64ba713a224b1f607410e94526
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-05-17 17:53:21 +0530

    Update disable.py

--- GroupMenter/modules/disable.py
+++ GroupMenter/modules/disable.py
@@ -2,9 +2,9 @@ import importlib
 from typing import Union
 
 from future.utils import string_types
-from MashaRoBot import dispatcher
-from MashaRoBot.modules.helper_funcs.handlers import CMD_STARTERS, SpamChecker
-from MashaRoBot.modules.helper_funcs.misc import is_module_loaded
+from GroupMenter import dispatcher
+from GroupMenter.modules.helper_funcs.handlers import CMD_STARTERS, SpamChecker
+from GroupMenter.modules.helper_funcs.misc import is_module_loaded
 from telegram import ParseMode, Update
 from telegram.ext import (
     CallbackContext,
@@ -20,12 +20,12 @@ FILENAME = __name__.rsplit(".", 1)[-1]
 # If module is due to be loaded, then setup all the magical handlers
 if is_module_loaded(FILENAME):
 
-    from MashaRoBot.modules.helper_funcs.chat_status import (
+    from GroupMenter.modules.helper_funcs.chat_status import (
         connection_status,
         is_user_admin,
         user_admin,
     )
-    from MashaRoBot.modules.sql import disable_sql as sql
+    from GroupMenter.modules.sql import disable_sql as sql
     from telegram.ext.dispatcher import run_async
 
     DISABLE_CMDS = []
@@ -160,7 +160,7 @@ if is_module_loaded(FILENAME):
         args = context.args
         chat = update.effective_chat
         if len(args) >= 1:
-            disable_module = "MashaRoBot.modules." + args[0].rsplit(".", 1)[0]
+            disable_module = "GroupMenter.modules." + args[0].rsplit(".", 1)[0]
 
             try:
                 module = importlib.import_module(disable_module)
@@ -235,7 +235,7 @@ if is_module_loaded(FILENAME):
         chat = update.effective_chat
 
         if len(args) >= 1:
-            enable_module = "MashaRoBot.modules." + args[0].rsplit(".", 1)[0]
+            enable_module = "GroupMenter.modules." + args[0].rsplit(".", 1)[0]
 
             try:
                 module = importlib.import_module(enable_module)
