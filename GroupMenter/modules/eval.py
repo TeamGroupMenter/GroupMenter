866d84e9eb7fd9ca325e4735920503be3a3f3593
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-05-17 17:48:03 +0530

    Update eval.py

--- GroupMenter/modules/eval.py
+++ GroupMenter/modules/eval.py
@@ -6,8 +6,8 @@ import textwrap
 import traceback
 from contextlib import redirect_stdout
 
-from MashaRoBot import LOGGER, dispatcher
-from MashaRoBot.modules.helper_funcs.chat_status import dev_plus
+from GroupMenter import LOGGER, dispatcher
+from GroupMenter.modules.helper_funcs.chat_status import dev_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
@@ -76,7 +76,7 @@ def do(func, bot, update):
 
     os.chdir(os.getcwd())
     with open(
-        os.path.join(os.getcwd(), "MashaRoBot/modules/helper_funcs/temp.txt"), "w"
+        os.path.join(os.getcwd(), "GroupMenter/modules/helper_funcs/temp.txt"), "w"
     ) as temp:
         temp.write(body)
 
