d863e1568c5cb36a10ac8af0691f4b8742f320b1
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-05-19 17:12:14 +0530

    Update locks_sql.py

--- GroupMenter/modules/sql/locks_sql.py
+++ GroupMenter/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from MashaRoBot.modules.sql import SESSION, BASE
+from GroupMenter.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
