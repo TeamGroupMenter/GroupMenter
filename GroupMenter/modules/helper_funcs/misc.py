ea78dd26705a1a40749e0303209e27a23f550a17
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-05-19 15:09:36 +0530

    Update misc.py

--- GroupMenter/modules/helper_funcs/misc.py
+++ GroupMenter/modules/helper_funcs/misc.py
@@ -1,6 +1,6 @@
 from typing import Dict, List
 
-from MashaRoBot import NO_LOAD
+from GroupMenter import NO_LOAD
 from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, ParseMode
 from telegram.error import TelegramError
 
