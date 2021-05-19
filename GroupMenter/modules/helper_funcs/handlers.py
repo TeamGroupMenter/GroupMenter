291fbf8f9f713c14f45307fd72c70aafc3c8cb91
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-05-19 15:18:54 +0530

    Update handlers.py

--- GroupMenter/modules/helper_funcs/handlers.py
+++ GroupMenter/modules/helper_funcs/handlers.py
@@ -1,6 +1,6 @@
-import MashaRoBot.modules.sql.blacklistusers_sql as sql
-from MashaRoBot import ALLOW_EXCL
-from MashaRoBot import DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES
+import  GroupMenter.modules.sql.blacklistusers_sql as sql
+from GroupMenter import ALLOW_EXCL
+from GroupMenter import DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES
 
 from telegram import Update
 from telegram.ext import CommandHandler, MessageHandler, RegexHandler, Filters
