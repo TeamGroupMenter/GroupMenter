b5f413dbf9f25dea57306382f83a8adee3249a43
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-05-19 07:50:51 +0530

    Update __cricketscore.py

--- GroupMenter/modules/__cricketscore.py
+++ GroupMenter/modules/__cricketscore.py
@@ -1,11 +1,8 @@
-# credits  @chsaiujwal @daisyxrobot
-# ported to masha @Mr_Dark_Prince
-
 import urllib.request
 
 from bs4 import BeautifulSoup
 from telethon import events
-from MashaRoBot import telethn as tbot
+from GroupMenter import telethn as tbot
 from telethon.tl import functions, types
 from telethon.tl.types import *
 
