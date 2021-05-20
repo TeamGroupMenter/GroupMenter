d0dae1e0aa22424035daacc2c6605a53745c2ed0
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-05-20 09:20:53 +0530

    Update __init__.py

--- GroupMenter/modules/sql/__init__.py
+++ GroupMenter/modules/sql/__init__.py
@@ -1,4 +1,4 @@
-from MashaRoBot import DB_URI
+from GroupMenter import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
