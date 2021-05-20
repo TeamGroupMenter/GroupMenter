4b910574412b41495d6b0ed089850f61df632ff5
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-05-20 09:15:54 +0530

    Update connection_sql.py

--- GroupMenter/modules/sql/connection_sql.py
+++ GroupMenter/modules/sql/connection_sql.py
@@ -4,7 +4,7 @@ from typing import Union
 
 from sqlalchemy import Column, String, Boolean, UnicodeText, Integer
 
-from MashaRoBot.modules.sql import SESSION, BASE
+from GroupMenter.modules.sql import SESSION, BASE
 
 
 class ChatAccessConnectionSettings(BASE):
