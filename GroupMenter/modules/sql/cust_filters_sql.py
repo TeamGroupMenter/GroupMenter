ee908a38c09ce269f0ceb7d06e9e39df9a9e94e7
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-05-20 09:15:28 +0530

    Update cust_filters_sql.py

--- GroupMenter/modules/sql/cust_filters_sql.py
+++ GroupMenter/modules/sql/cust_filters_sql.py
@@ -2,8 +2,8 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Boolean, Integer, distinct, func
 
-from MashaRoBot.modules.helper_funcs.msg_types import Types
-from MashaRoBot.modules.sql import BASE, SESSION
+from GroupMenter.modules.helper_funcs.msg_types import Types
+from GroupMenter.modules.sql import BASE, SESSION
 
 
 class CustomFilters(BASE):
