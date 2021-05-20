b5dd7db179183ded4b0fb7a1cecfc2c5f54443ea
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-05-20 09:18:21 +0530

    Update blacklist_sql.py

--- GroupMenter/modules/sql/blacklist_sql.py
+++ GroupMenter/modules/sql/blacklist_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import func, distinct, Column, String, UnicodeText, Integer
 
-from MashaRoBot.modules.sql import SESSION, BASE
+from GroupMenter.modules.sql import SESSION, BASE
 
 
 class BlackListFilters(BASE):
