8e23293e0bd06183690f4c0c4e84b2d7ae16199e
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-05-22 10:14:24 +0530

    Update __main__.py

--- GroupMenter/__main__.py
+++ GroupMenter/__main__.py
@@ -74,11 +74,9 @@ def get_readable_time(seconds: int) -> str:
 
 
 PM_START_TEXT = """
-Hey there! My name is **Group Menter 🤖**.
-I'm here to help you manage your groups!
+*Hey there! My name is Group Menter 🤖*.
+*I'm here to help you manage your groups!*
 Hit /help to find out more about how to use me to my full potential.
-
-Join My [News Channel](https://t.me/GroupMenterUpdates) to get information on all the latest updates.
 """
 
 buttons = [
