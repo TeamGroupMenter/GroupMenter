cbd7eb7fd7e7b53e3617bd62b0815f563dcfc49b
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-05-27 14:49:25 +0530

    Update gm.log.go

--- DMCA/gm.log.go
+++ DMCA/gm.log.go
@@ -1,15 +1,1 @@
->>>>>>>>>>>Group Menter<<<<<<<<<<
 
-■■■■■■■■■■□□□□□□□□   
-》》》》》》》》》》》》》》》》》》》》◇◇◇◇◇
-
-
-gm.log.py3.9.2.py_get'log'
-@tg.client-tgtg.
-update.time_ping
-in_runtime.py
-GM.tg.
-update.file_name("gm.log.py3.9.2.py")**
-from import)*
-from telegram.app.import.files/bot/search=groupmenterrobot;sel_uptime
-     tg.open.up
