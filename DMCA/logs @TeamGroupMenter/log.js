c3c515bad9fe5c5d6ff626a8021c47424abe8ee8
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-05-27 14:50:21 +0530

    Create log.js

---
+++ DMCA/logs @TeamGroupMenter/log.js
@@ -0,0 +1,30 @@
+>>>>>>>>>>>Group Menter<<<<<<<<<<
+
+■■■■■■■■■■□□□□□□□□   
+》》》》》》》》》》》》》》》》》》》》◇◇◇◇◇
+
+
+gm.log.py3.9.2.py_get'log'
+@tg.client-tgtg.
+update.time_ping
+in_runtime.py
+GM.tg.
+update.file_name("gm.log.py3.9.2.py")**
+from import)*
+from telegram.app.import.files/bot/search=groupmenterrobot;sel_uptime
+     tg.open.up
+             #uptime!!!
+8.180
+118.1
+wow1I the 
+91881
+
+1p1k1
+Lpkq
+qai
+Laim
+Ala
+aj19161916187owua282n28wwv
+Na a
+Oa
+Ajaoa
