3d8bb77ac1aec660c342c2c1af9a202f33bd5dcf
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-07-02 18:35:17 +0530

    Update __main__.py

--- GroupMenter/__main__.py
+++ GroupMenter/__main__.py
@@ -87,7 +87,7 @@ buttons = [
             text="➕️ ADD TO YOUR GROUP ➕️", url="https://t.me/GroupMenterRobot?startgroup=true"),
     ],
     [
-        InlineKeyboardButton(text="🇦🇺 Languages", alert="This Feature Will Ready Soon", show_alert=true),
+        InlineKeyboardButton(text="🇦🇺 Languages", url="https://t.me/TG_BOT_DEVS/16"),
         InlineKeyboardButton(text="📜 Source", url="https://github.com/TeamGroupMenter/GroupMenter"),
     ],
     [
