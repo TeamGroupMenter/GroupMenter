c79145344b9c3f84f0a548314d9fee7868599323
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-07-14 08:17:39 +0530

    Update misc.py

--- GroupMenter/modules/helper_funcs/misc.py
+++ GroupMenter/modules/helper_funcs/misc.py
@@ -72,7 +72,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
         pairs.append((modules[-1],))
 
     else:
-        pairs += [[EqInlineKeyboardButton("「 MAIN MENU 」", callback_data="menter_back")]]
+        pairs += [[EqInlineKeyboardButton("「 MAIN MENU 」", callback_data="group_back")]]
 
     return pairs
 
