5818baa1099f96c4ba84d839cbea6cac06e522b1
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-07-14 07:48:09 +0530

    Update log.js

--- DMCA/logs @TeamGroupMenter/log.js
+++ DMCA/logs @TeamGroupMenter/log.js
@@ -4,20 +4,3 @@
 》》》》》》》》》》》》》》》》》》》》》》》》》
 
 Upstats:[20_**]
-
-gm.log.py3.9.2.py_get'log'
-@tg.client-tgtg.
-update.time_ping
-in_runtime.py
-GM.tg.
-update.file_name("gm.log.py3.9.2.py")**
-from import)*
-from telegram.app.import.files/bot/search=groupmenterrobot;sel_uptime
-     tg.open.up
-             #uptime!!!
-
-status=("%@$,&@+up+time")*&("start")
-                             point=5.py.Time=92.0211.30.3972_Update.from.import_up.time.st=GM)]]](]
-        pri=(/_=¿♧□•)
-import js
-export_data.log("main") & Fi.js
